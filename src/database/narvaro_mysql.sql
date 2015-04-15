ee40577141c6d2849291ce281405195e31da5ee4
Jason Sipula <alupis1@gmail.com>
2015-04-14 19:24:51 -0700

    Implement basic schema, ref #20

--- src/database/narvaro_mysql.sql
+++ src/database/narvaro_mysql.sql
@@ -7,7 +7,10 @@
 # ANT: @ANT@
 #
 
-CREATE TABLE narvaroVersion (
+# Table holds Narvaro version data
+#   Used for schema upgrades
+CREATE TABLE narvaroVersion
+(
     name    VARCHAR(50) NOT NULL,
     version INTEGER NOT NULL,
     PRIMARY KEY (name)
@@ -15,3 +18,63 @@ CREATE TABLE narvaroVersion (
 
 # insert default table values
 INSERT INTO narvaroVersion (name, version) VALUES ('narvaro', 1);
+
+# Table holds park names
+CREATE TABLE parks
+(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) UNIQUE
+);
+
+# Table holds 449 Form binary blobs
+CREATE TABLE forms
+(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    filename VARCHAR(80),
+    form MEDIUMBLOB
+);
+
+# This table holds monthly park data
+#
+# Prefix Key:
+#     pdu = Paid Day Use
+#     fdu = Free Day Use
+#     fsc = Free Safety Classes
+#     o   = Other
+CREATE TABLE data
+(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    park INT UNSIGNED,
+    month DATE,
+    pduConversionFactor DECIMAL(8,4),
+    pduTotals INT UNSIGNED,
+    pduSpecialEvents INT UNSIGNED,
+    pduAnnualDayUse INT UNSIGNED,
+    pduDayUse INT UNSIGNED,
+    pduSenior INT UNSIGNED,
+    pduDisabled INT UNSIGNED,
+    pduGoldenBear INT UNSIGNED,
+    pduDisabledVeteran INT UNSIGNED,
+    pduNonResOHVPass INT UNSIGNED,
+    pduAnnualPassSale INT UNSIGNED,
+    pduCamping INT UNSIGNED,
+    pduSeniorCamping INT UNSIGNED,
+    pduDisabledCamping INT UNSIGNED,
+    fduConversionFactor DECIMAL(8,4),
+    fduTotals INT UNSIGNED,
+    fscTotalVehicles INT UNSIGNED,
+    fscTotalPeople INT UNSIGNED,
+    fscRatio DECIMAL(8,4),
+    oMC INT UNSIGNED,
+    oATV INT UNSIGNED,
+    o4X4 INT UNSIGNED,
+    oROV INT UNSIGNED,
+    oAQMA INT UNSIGNED,
+    oAllStarKarting INT UNSIGNED,
+    oHangtown INT UNSIGNED,
+    oOther INT UNSIGNED,
+    comment TEXT,
+    form449 INT UNSIGNED,
+    FOREIGN KEY (park) REFERENCES (parks),
+    FOREIGN KEY (form449) REFERENCES (forms)
+);
