2bbd884b38f5a22d3e5148cbd7cf87abe48f6ef6
Jason Sipula <alupis1@gmail.com>
2015-04-16 15:53:05 -0700

    Add controller for setup page

---
+++ src/java/edu/csus/ecs/moneybeets/narvaro/ui/NarvaroSetup.java
@@ -0,0 +1,101 @@
+package edu.csus.ecs.moneybeets.narvaro.ui;
+
+import java.util.zip.DataFormatException;
+
+import org.apache.log4j.Logger;
+
+import edu.csus.ecs.moneybeets.narvaro.database.DatabaseType;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * This class acts as the controller for the 
+ * first-time setup menu.
+ *
+ */
+public class NarvaroSetup {
+    
+    private static final Logger LOG = Logger.getLogger(NarvaroSetup.class.getName());
+    
+    @FXML private MenuButton databaseTypeSelector;
+    @FXML private TextField serverName;
+    @FXML private TextField portNumber;
+    @FXML private TextField databaseUser;
+    @FXML private PasswordField databasePassword;
+
+    /**
+     * This method is invoked when the OK button is pressed.
+     * 
+     * @param event The event.
+     */
+    @FXML
+    public void handleOKButton(final ActionEvent event) {
+        if (validate()) {
+            writeConfig();
+        }
+        
+    }
+    
+    private boolean validate() {
+        return true;
+    }
+    
+    private void writeConfig() {
+        
+    }
+
+    /**
+     * @return the Database type.
+     * @throws DataFormatException If selected option is not a valid db type.
+     */
+    public DatabaseType getDatabaseType() throws DataFormatException {
+        for (MenuItem item : databaseTypeSelector.getItems()) {
+            if (((CheckMenuItem)item).isSelected()) {
+                return DatabaseType.valueOf(item.getText().toLowerCase());
+            }
+        }
+        // this should never happen
+        throw new DataFormatException("Invalid database type");
+    }
+
+    /**
+     * @return the serverName
+     */
+    private String getServerName() {
+        return serverName.getText();
+    }
+
+    /**
+     * @return the portNumber
+     */
+    private int getPortNumber() {
+        int port = -1;
+        try {
+            port = Integer.parseInt(portNumber.getText());
+        } catch (Exception e) {
+            LOG.error("Port number is invalid: " + portNumber.getText());
+            LOG.error("Using default port for database type");
+        }
+        return port;
+    }
+
+    /**
+     * @return the databaseUser
+     */
+    private String getDatabaseUser() {
+        return databaseUser.getText();
+    }
+
+    /**
+     * @return the databasePassword
+     */
+    private String getDatabasePassword() {
+        return databasePassword.getText();
+    }
+    
+}
