20d974233f25c450097416d9d8d853c20864fded
Jason Sipula <alupis1@gmail.com>
2015-04-16 20:05:31 -0700

    Add database server names and fromString() method

--- src/java/edu/csus/ecs/moneybeets/narvaro/database/DatabaseType.java
+++ src/java/edu/csus/ecs/moneybeets/narvaro/database/DatabaseType.java
@@ -9,16 +9,51 @@
 
 package edu.csus.ecs.moneybeets.narvaro.database;
 
+import java.util.zip.DataFormatException;
+
 /**
  * Narvaro supported database types.
  *
  */
 public enum DatabaseType {
 
-    sqlserver,
+    sqlserver("SQL Server"),
     
-    mysql,
+    mysql("MySQL"),
     
     ;
+    
+    private DatabaseType(final String name) {
+    	this.name = name;
+    }
+    
+    final String name;
+    
+    /**
+     * @return The database name.
+     */
+    public String getName() {
+    	return name;
+    }
+    
+    /**
+     * Converts a string to the proper matching DatabaseType.
+     * 
+     * @param in The string.
+     * @return The database type if found.
+     * @throws DataFormatException If input is null or empty, or not found.
+     */
+    public static DatabaseType fromString(final String in) throws DataFormatException {
+    	if ("".equals(in) || in == null) {
+    		throw new DataFormatException("Input not found");
+    	}
+    	for (DatabaseType dbType : DatabaseType.values()) {
+    		if (in.equalsIgnoreCase(dbType.toString()) 
+    				|| in.equalsIgnoreCase(dbType.getName())) {
+    			return dbType;
+    		}
+    	}
+    	throw new DataFormatException("Input not found");
+    }
     
 }
