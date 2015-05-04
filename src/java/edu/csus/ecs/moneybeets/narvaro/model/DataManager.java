7e245485bedc9c56592661cbe8a8bc65c12a7051
Jason Sipula <alupis1@gmail.com>
2015-05-04 15:42:04 -0700

    Add utility method to convert YearMonth into java.sql.Date

--- src/java/edu/csus/ecs/moneybeets/narvaro/model/DataManager.java
+++ src/java/edu/csus/ecs/moneybeets/narvaro/model/DataManager.java
@@ -1,9 +1,12 @@
 package edu.csus.ecs.moneybeets.narvaro.model;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.YearMonth;
+import java.util.Calendar;
 
 import org.apache.log4j.Logger;
 
@@ -40,21 +43,32 @@ public enum DataManager {
     
     public final TimeSpan getTimeSpan(final YearMonth start, final YearMonth end) {
         setupConnection();
-        psSelectMonthData.setDate(1, start);
-        psSelectMonthData.setDate(2, end.);
+        ResultSet rs = null;
+        try {
+            psSelectMonthData.setDate(1, DataManager.convertYearMonthToDate(start));
+            psSelectMonthData.setDate(2, DataManager.convertYearMonthToDate(end));
+            rs = psSelectMonthData.executeQuery();
+            
+            while (rs.next()) {
+                
+            }
+            
+        } catch (SQLException e) {
+            
+        }
     }
     
-    public final TimeSpan getTimeSpanForPark(
-            final YearMonth start, final YearMonth end, final String parkName) {
-        setupConnection();
-        
-    }
+    //public final TimeSpan getTimeSpanForPark(
+    //        final YearMonth start, final YearMonth end, final String parkName) {
+    //    setupConnection();
+    //    
+    //}
     
-    public final TimeSpan getTimeSpanForParks(
-            final YearMonth start, final YearMonth end, final String ... parkNames) {
-        setupConnection();
-        
-    }
+    //public final TimeSpan getTimeSpanForParks(
+    //        final YearMonth start, final YearMonth end, final String ... parkNames) {
+    //    setupConnection();
+    //    
+    //}
     
     private void setupConnection() {
         try {
@@ -71,6 +85,28 @@ public enum DataManager {
             LOG.error(e.getMessage(), e);
         }
 
+    }
+    
+    /**
+     * Utility method to convert a <code>YearMonth</code> into a <code>Date</code>.
+     * 
+     * @param ym The <code>YearMonth</code> to convert.
+     * @return The converted date.
+     */
+    public static Date convertYearMonthToDate(final YearMonth ym) {
+        Calendar cal = Calendar.getInstance();
+        cal.clear();
+        // Gregorian calendar has months 0 - 11, 0 being January.
+        //   java.time is more sane and has months 1 - 12, 1 being January.
+        //   So we must convert them here...
+        cal.set(Calendar.MONTH, (ym.getMonthValue() - 1));
+        // Since we cannot have a date object without a day, we must pick one
+        //    or the system will use today, whatever day of the month that happens to be.
+        //  So to avoid situations where it's the 30th day of the month, but the user is
+        //    trying to work with February (29 days), we should pick a sane default... the 1st.
+        cal.set(Calendar.DAY_OF_MONTH, 1);
+        cal.set(Calendar.YEAR, ym.getYear());
+        return (Date) cal.getTime();
     }
     
     
